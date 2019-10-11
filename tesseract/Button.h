cd08fa239ceb6bb5f5a04ebf23583d02bb97c281
alakiza <Tigr.i.T34@yandex.ru>
2019-10-11 20:34:16 +0300

    Add Button class

--- tesseract/Button.h
+++ tesseract/Button.h
@@ -4,16 +4,64 @@
 	class Button
 	{
 	private:
-		int fpin;
-	public:
-		Button(const int pin)
-		{
+		char fpin;
 
+		char fPressCount;
+		char fMaxPressCountToValid;
+		bool fState;
+
+		bool fKeyPress;
+
+		inline void BaseInit(const char& pin)
+		{
+			fpin = pin;
+			fKeyPress = false;
+			fPressCount = 0;
+
+			pinMode(fpin, INPUT);
 		}
 
-		bool KeyPressed()
+	public:
+
+		Button(const char pin)
 		{
-			
+			BaseInit(pin);
+			fMaxPressCountToValid = 15;
+		}
+
+		Button(const char pin, const char MaxPressCountToValid)
+		{
+			BaseInit(pin);
+			fMaxPressCountToValid = MaxPressCountToValid;
+		}
+
+		void ScanState()
+		{
+			if ( fState == (!digitalRead(fpin)) ) 
+			{
+			    fPressCount = 0;
+			}
+			else 
+			{
+			    ++fPressCount;
+
+			    if ( fPressCount >= fMaxPressCountToValid ) 
+			    {
+            		fKeyPress = fState;
+			      	fState = !fState;
+			      	fPressCount = 0;
+			    }   
+			}
+		}
+
+		bool Pressed()
+		{
+			return fKeyPress;
+		}
+
+		char pin()
+		{
+			return fpin;
 		}
 
 		~Button()
--- tesseract/tesseract.ino
+++ tesseract/tesseract.ino
@@ -1,12 +1,33 @@
 #include "Visualizer.h"
 #include "Button.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  
+#define BUTTON_COUNT 3
+
+#define BUTTON_LEFT  0
+#define BUTTON_RIGHT 1
+#define BUTTON_ENTER 2
+
+Button** buttons;
+
+void ScanButtons()
+{
+	for(char i = 0; i < BUTTON_COUNT; ++i) buttons[i]->ScanState();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() 
+{
+  buttons = new Button*[BUTTON_COUNT];
+  for(char i = 0; i < BUTTON_COUNT; ++i) buttons[i] = new Button(4+i);
 
+  pinMode(13, OUTPUT);
+}
+
+void loop() 
+{
+	ScanButtons();
+	if(buttons[BUTTON_LEFT]->Pressed()) 
+		digitalWrite(13, HIGH);
+	else
+		digitalWrite(13, LOW);
+   delay(1);
 }
